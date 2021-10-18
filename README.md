7f59104e4143925fc88b484e1fef825b544f1849
Jan Van den Audenaerde <jan.vandenaudenaerde@gmail.com>
2021-10-18 09:52:57 +0200

    software correction of raw pulses

--- README.md
+++ README.md
@@ -7,6 +7,3 @@
 * [binnes/esp8266Workshop](https://binnes.github.io/esp8266Workshop/index.html)
   * [setup for the workshop](https://binnes.github.io/esp8266Workshop/part1/PREREQ.html)
   * [your first ESP8266 application](https://binnes.github.io/esp8266Workshop/part1/FIRSTAPP.html)
-
-
-  
--- esp8266_water_meter.ino
+++ esp8266_water_meter.ino
@@ -1,26 +1,20 @@
-#include <ESP8266WiFiGratuitous.h>
-#include <WiFiServerSecure.h>
-#include <WiFiClientSecure.h>
-#include <ArduinoWiFiServer.h>
-#include <WiFiClientSecureBearSSL.h>
-#include <ESP8266WiFi.h>
-#include <ESP8266WiFiMulti.h>
-#include <WiFiUdp.h>
-#include <ESP8266WiFiType.h>
-#include <CertStoreBearSSL.h>
-#include <ESP8266WiFiAP.h>
-#include <WiFiClient.h>
-#include <BearSSLHelpers.h>
-#include <WiFiServer.h>
-#include <ESP8266WiFiScan.h>
-#include <WiFiServerSecureBearSSL.h>
-#include <ESP8266WiFiGeneric.h>
-#include <ESP8266WiFiSTA.h>
-
 /*
- Created by Jan Van den Audenaerde
- Based on some ESP8266 and PubSub code examples 
- 
+ Author Jan Van den Audenaerde
+ Date 2018-10-08
+
+ This sketch is used to measure the water consumption of an Elster V100 (15 or 20mm) meter
+ https://www.elstermetering.com/assets/products/products_elster_files/ELS10138_V100_Specsheet_V2_SV.pdf
+ using a reed switch.
+ According those specs: this water meter model generates a pulse every 0.5 l and has a maximum flow rate of 5 m3/h.
+ In other words we maximally can expect 2.78 pulses/sec   (= 5 m3/h * 1000 l/m3 * 1h/3600 sec * 1 pulse/0.5 l)
+ Or the minimum time between 2 successive pulses is 360 millis.
+ => therefore we have set our debouncing time to 100 millis (most likely we can set it even higher).
+
+ This sketch is Based on 
+   - some ESP8266 and PubSub code examples 
+   - interrupt handler debouncing logic from Delphiño K.M.
+     (see https://forum.arduino.cc/index.php?topic=466640.0 )
+
 */
 
 #include <ESP8266WiFi.h>
@@ -28,9 +22,9 @@
 #include <WiFiClient.h> 
 #include "config.h"  // defines all constants starting with CFG_
 
-const char* ssid = CFG_WIFI_SSID ;  
-const char* password = CFG_WIFI_PASSWORD;
-const char* host = CFG_HOST;
+const char* ssid      = CFG_WIFI_SSID ;  
+const char* password  = CFG_WIFI_PASSWORD;
+const char* host      = CFG_HOST;
 
 const char* mqttServer = CFG_MQTT_SERVER;
 WiFiClient   espClient;
@@ -41,12 +35,28 @@ char mqttPublishMsg[50];
 // regarding GPIO pins functions https://tttapa.github.io/ESP8266/Chap04%20-%20Microcontroller.html
 int reedSwitchPin = 4;   // reed switch is connected GPIO4 = D2 (https://circuits4you.com/2017/12/31/nodemcu-pinout/)
 
+/***************  INTERRUPT HANDLER ***********************************/
 volatile int pulseCount = 0;
+volatile int rawPulseCount = 0;  // this is the pulseCount we get without the software debouncing
 
-void ICACHE_RAM_ATTR  onWaterMeterPulse() {
-    pulseCount++;
-    // Serial.println(pulseCount);
+volatile unsigned long lastPulseTimeStamp = 0;
+unsigned long debouncingTime = 100*1000; //  Debouncing Time in Microseconds - set to 0.1 second.
+
+void ICACHE_RAM_ATTR onWaterMeterPulse() {
+   unsigned long current_time; // current time in micro seconds.
+   current_time=micros();  // rolls over every 71.6 minutes
+      
+   rawPulseCount++;
+   
+   // A (rising) pulse is only counted it the previous counted pulse happened more than debouncingTime microseconds ago
+   // overflow is taken into account as specified in https://arduino.stackexchange.com/questions/12587/how-can-i-handle-the-millis-rollover ))
+   if( (current_time - lastPulseTimeStamp) >= debouncingTime ){
+      pulseCount++;
+      lastPulseTimeStamp = current_time;
+      // Serial.println(pulseCount);
+    }
 }
+/*************** END OF INTERRUPT HANDLER ***********************************/
 
 void setup() {
     Serial.begin(115200);
@@ -54,7 +64,7 @@ void setup() {
     Serial.println("\nPreparing the reed switch water meter project...");
     
     pinMode(reedSwitchPin, INPUT_PULLUP);
-    attachInterrupt(digitalPinToInterrupt(reedSwitchPin), onWaterMeterPulse, CHANGE);
+    attachInterrupt(digitalPinToInterrupt(reedSwitchPin), onWaterMeterPulse, RISING);
 
     // setting up wifi connnection
     Serial.println();
@@ -114,5 +124,9 @@ void loop() {
     // Serial.println(pulseCount);
     snprintf(mqttPublishMsg, 50, "%d", pulseCount);
     mqttClient.publish(CFG_MQTT_TOPIC_WATER_METER_PULSE_COUNT , mqttPublishMsg);
+
+    // publishing the water meter raw pulse count   
+    snprintf(mqttPublishMsg, 50, "%d", rawPulseCount);
+    mqttClient.publish(CFG_MQTT_TOPIC_WATER_METER_RAW_PULSE_COUNT , mqttPublishMsg);
   }
 }
